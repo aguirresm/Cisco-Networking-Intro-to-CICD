2f7a34124659cf8d770d6f7960fdd553c5a00ca8
Neython Lec Streitz <nlecstre@cisco.com>
2026-04-05 15:53:42 -0700

    Modified testbed path

--- tests/pre_check.py
+++ tests/pre_check.py
@@ -4,7 +4,7 @@ import os
  
 # Load the testbed
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-testbed = load(os.path.join(BASE_DIR, "testbed/lab_testbed.yaml"))
+testbed = load(os.path.join(BASE_DIR, "tests/testbed/lab_testbed.yaml"))
  
 snapshot = {}
  
